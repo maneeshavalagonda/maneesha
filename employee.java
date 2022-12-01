51acc7b02db6c29a04d7b87fb5c7c2d6c24bd3e8
maneeshavalagonda <119621086+maneeshavalagonda@users.noreply.github.com>
2022-12-01 23:29:45 +0530

    Add files via upload

---
+++ employee.java
@@ -0,0 +1,51 @@
+package com.maneesha.com.maneesha.example1;
+import java.util.*;
+public class employee {
+	 private int employee_id;
+	 private String employee_name,employee_companyname,employee_department,employee_gender,employee_address;
+	 private float employee_salary;
+	 public int getemployee_id() {
+			return employee_id;
+	 }
+	 public void setemployee_id(int employee_id) {
+		 this.employee_id=employee_id;
+	 }
+	public String getemployee_name() {
+		    return employee_name;
+	}
+		 public void setemployee_name(String employee_name) {
+			 this.employee_name=employee_name;
+		 }
+		 public String getemployee_companyname() {
+			return employee_companyname ;
+		 }
+		 public void setemployee_companyname(String employee_companyname) {
+			 this.employee_companyname=employee_companyname;
+		 }
+		 public String getemployee_department() {
+				return employee_department;
+		 }
+		 public void setemployee_department(String employee_department) {
+			 this.employee_department=employee_department;
+		 }
+		 
+		 public String getemployee_gender() {
+			return employee_gender;
+		 }
+		 public void setemployee_gender(String employee_gender) {
+			 this.employee_gender=employee_gender;
+		 }
+		 public String getemployee_address() {
+			return employee_address;
+		 }
+		 public void setemployee_address(String employee_address) {
+			 this.employee_address=employee_address;
+		 }
+		 public Float getemployee_salary() {
+			return employee_salary;
+		 }
+			public void setemployee_salary(float employee_salary) {
+				 this.employee_salary=employee_salary;	
+		 }
+		}
+	
